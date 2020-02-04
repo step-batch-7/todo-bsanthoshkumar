0aecfcfc208500a7308cc3be3e9aa5090c8bc95d
santhosh <santhosh.kumar@thoughtworks.com>
2020-02-04 13:06:15 +0530

    added showing todos as each division

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,22 +1,61 @@
-const getElement = id => document.getElementById(id);
 const createElement = elementName => document.createElement(elementName);
+const getElement = selector => document.querySelector(selector);
 
 const togglePopUp = () => {
-  const popUpDivision = getElement('popUpDivision');
+  const popUpDivision = getElement('#popUpDivision');
   const displayValue = popUpDivision.style.display;
   popUpDivision.style.display = displayValue === 'block' ? 'none' : 'block';
   return;
 };
 
-const addTodoList = () => {
-  const mainDivision = getElement('mainDivision');
-  const title = getElement('title').value;
-  const newTodo = createElement('div');
-  const heading = createElement('h4');
-  heading.innerText = title;
-  newTodo.className = 'todoList';
-  newTodo.appendChild(heading);
-  mainDivision.prepend(newTodo);
-  togglePopUp();
-  return;
+const sendHttpGet = (url, callback) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.onload = function() {
+    if (this.status === 200) {
+      callback(this.responseText);
+    }
+  };
+  request.send();
+};
+
+const createHeader = title => {
+  const header = createElement('h4');
+  header.classList.add('heading');
+  header.innerText = title;
+  return header;
+};
+
+const createTask = item => {
+  const { value, isDone } = item;
+  const task = document.createElement('p');
+  task.innerHTML = value + '<br/>';
+  return task;
+};
+const createTodoList = todoList => {
+  const { id, title, items } = todoList;
+  console.log(todoList);
+  const todoDivision = document.createElement('div');
+  const header = createHeader(title);
+  const line = createElement('hr');
+  todoDivision.id = id;
+  todoDivision.classList.add('todoList');
+  todoDivision.append(header);
+  todoDivision.append(line);
+  items.map(item => todoDivision.append(createTask(item)));
+  return todoDivision;
+};
+
+const showTodoLists = text => {
+  const container = getElement('#container');
+  const todoLists = JSON.parse(text);
+  const todoListsAsHtml = todoLists.map(createTodoList);
+  container.innerHTML = '';
+  todoListsAsHtml.forEach(todoList => container.append(todoList));
+};
+
+const loadTodoLists = () => sendHttpGet('/getTodoLists', showTodoLists);
+
+const main = () => {
+  loadTodoLists();
 };
